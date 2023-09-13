d586e70694ba075ba6b65e4173fcdb5fa8513404
Asad Siddiqui <asad@howeneat.com>
2023-09-12 23:48:23 -0700

    hero section completed

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,10 +1,15 @@
- import React from 'react'
+"use client";
+
+import Image from "next/image"
+import { CustomButtonProps } from "@/types";
  
- const CustomButton = () => {
+ const CustomButton = ({title, containerStyles, handleClick}: CustomButtonProps) => {
    return (
-     <div>
-       
-     </div>
+     <button disabled={false} type={"button"} className={`custom-btn ${containerStyles}`} onClick={handleClick}>
+      <span className={`flex-1`}>
+        {title}
+      </span>
+     </button>
    )
  }
  
