8a498bd6b87864d3a036910720563a9b72066d9b
Asad Siddiqui <asadsidd22@outlook.com>
2023-09-13 21:36:17 -0700

    hero section complete

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,39 +19,13 @@ const Hero = () => {
       </div>
       <div className='hero__image-container'>
         <div className='hero__image'>
-          {/* <Image src={""} /> */}
+          <Image src="/hero.png" alt="Hero" fill className='object-contain'/>
 
-
+          <div className='hero__image-overlay' />
         </div>
       </div>
-      <div className='hero__image-container'>
-        <div className='hero__image'>
-          {/* <Image src={""} /> */}
 
-          
-        </div>
-      </div>
-      <div className='hero__image-container'>
-        <div className='hero__image'>
-          {/* <Image src={""} /> */}
 
-          
-        </div>
-      </div>
-      <div className='hero__image-container'>
-        <div className='hero__image'>
-          {/* <Image src={""} /> */}
-
-          
-        </div>
-      </div>
-      <div className='hero__image-container'>
-        <div className='hero__image'>
-          {/* <Image src={""} /> */}
-
-          
-        </div>
-      </div>
       
     </div>
   )
