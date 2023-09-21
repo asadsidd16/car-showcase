d2909ff0d4ffa1c834969ccbac580fb13d608c62
Asad Siddiqui <asadsidd22@outlook.com>
2023-09-20 21:50:19 -0700

    search bar props added

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
-
+"use client";
+import SearchManufacturer from "@/components/SearchManufacturer";
+import { useState } from "react";
 const SearchBar = () => {
+  const [manufacturer, setManufacturer] = useState("");
+  const handleSearch = () => {};
   return (
-    <div>
-      SearchBar
-    </div>
-  )
-}
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer manufacturer={manufacturer} setManufacturer={setManufacturer}/>
+      </div>
+    </form>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
