54d4c8c0372d5497a565a4454d974aba04fa7e2c
Asad Siddiqui <asadsidd22@outlook.com>
2023-09-22 19:49:42 -0700

    cars api added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
 import { CustomFilter, Hero, SearchBar } from "@/components";
+import { fetchCars } from "@/utils";
 
-export default function Home() {
+export default async function Home() {
+
+  const allCars = await fetchCars()
+  console.log("CARS", allCars)
   return (
     <main className="overflow-hidden">
       <Hero />
