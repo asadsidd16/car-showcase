ad6aaf38d12b077e464aa8a570d401e66e5cb3f8
Asad Siddiqui <asadsidd22@outlook.com>
2023-09-25 00:11:11 -0700

    display cards

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default async function Home() {
           </div>
         </div>
         {!isDataEmpty ? (
-          <section>WE HAVE CARS</section>
+          <section><div className="home__cars-wrapper">{allCars?.map((car) => <CarCard car={car}/>)}</div></section>
         ) : (
           <div className="home__error-container">
             <h2 className="text-black text-xl font-bold">Oops, no results</h2>
