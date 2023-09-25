647260cb015dccd240373e8179c0017e72e56874
Asad Siddiqui <asadsidd22@outlook.com>
2023-09-25 00:10:45 -0700

    car error message

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,11 @@ import { CustomFilter, Hero, SearchBar } from "@/components";
 import { fetchCars } from "@/utils";
 
 export default async function Home() {
+  const allCars = await fetchCars();
+  console.log("CARS", allCars);
 
-  const allCars = await fetchCars()
-  console.log("CARS", allCars)
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -14,13 +16,21 @@ export default async function Home() {
           <h1 className="text-4xl font-extrabold">Car Catalog</h1>
           <p>Explore the cars you might like</p>
         </div>
-        <div className="home__filters"> 
-        <SearchBar />
-        <div className="home__filter-container">
-          <CustomFilter title="fuel"/>
-          <CustomFilter title="year"/>
+        <div className="home__filters">
+          <SearchBar />
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
         </div>
-        </div>
+        {!isDataEmpty ? (
+          <section>WE HAVE CARS</section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
