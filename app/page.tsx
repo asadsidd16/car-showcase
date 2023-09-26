04828b13084af2eb30892df1e0f5aff8b23df50a
Asad Siddiqui <asadsidd22@outlook.com>
2023-09-25 20:52:12 -0700

    car card setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { CustomFilter, Hero, SearchBar } from "@/components";
 import { fetchCars } from "@/utils";
+import {CarCard} from "@/components";
 
 export default async function Home() {
   const allCars = await fetchCars();
