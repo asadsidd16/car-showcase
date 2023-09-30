f3bc48c5579d0cb450d88355c77a73f8396bfaf6
Asad Siddiqui <asadsidd22@outlook.com>
2023-09-30 11:38:18 -0700

    car styling

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Image from "next/image";
 import { CarProps } from "@/types";
 import { calculateCarRent } from "@/utils";
+import { CustomButton } from ".";
 
 interface CarCardProps {
   car: CarProps;
@@ -23,6 +24,8 @@ const CarCard = ({ car }: CarCardProps) => {
     transmission,
     year,
   } = car;
+
+  const [isOpen, setIsOpen] = useState(false)
 
   const carRent = calculateCarRent(city_mpg, year);
   return (
@@ -46,6 +49,38 @@ const CarCard = ({ car }: CarCardProps) => {
           priority
           className="object-contain"
         />
+      </div>
+      <div className="relative flex w-full mt-2">
+        <div className="flex group-hover:invisible w-full justify-between text-gray">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image
+              src="/steering-wheel.svg"
+              width={20}
+              height={20}
+              alt="steering wheel"
+            />
+            <p className="text-[14px]">
+              {transmission === "a" ? "Automatic" : "Manual"}
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/tire.svg" width={20} height={20} alt="tire" />
+            <p className="text-[14px]">{drive.toUpperCase()}</p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/gas.svg" width={20} height={20} alt="steering wheel" />
+            <p className="text-[14px]">{city_mpg} MPG</p>
+          </div>
+        </div>
+        <div className="car-card__btn-container">
+          <CustomButton
+            title="View More"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setIsOpen(true)}
+          ></CustomButton>
+        </div>
       </div>
     </div>
   );
--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,17 +1,35 @@
 "use client";
 
-import Image from "next/image"
+import Image from "next/image";
 import { CustomButtonProps } from "@/types";
- 
- const CustomButton = ({title, containerStyles, handleClick}: CustomButtonProps) => {
-   return (
-     <button disabled={false} type={"button"} className={`custom-btn ${containerStyles}`} onClick={handleClick}>
-      <span className={`flex-1`}>
-        {title}
-      </span>
-     </button>
-   )
- }
- 
- export default CustomButton
- 
+
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+  textStyles,
+  rightIcon,
+}: CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={"button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
+    </button>
+  );
+};
+
+export default CustomButton;
