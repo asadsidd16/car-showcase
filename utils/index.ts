e1886b313210302ddee73df1617b549d75d55eba
Asad Siddiqui <asadsidd22@outlook.com>
2023-10-10 19:46:47 -0700

    api for car

--- utils/index.ts
+++ utils/index.ts
@@ -29,5 +29,9 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 };
 
 export const generateCarImageUrl = (car: CarProps, angle?: string) => {
-    
+    const url = new URL('https://cdn.imagin.studio/getimage');
+
+    const { make, year, model} = car;
+
+    url.searchParams.append('customer', 'hrjavascript-mastery');
 }
