c3f7671ddbeef2e5522a1c70b21f296952dfc408
Asad Siddiqui <asadsidd22@outlook.com>
2023-11-13 21:21:06 -0800

    search functionalitry

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,6 +2,8 @@
 import SearchManufacturer from "@/components/SearchManufacturer";
 import { useState } from "react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
     <Image
@@ -14,9 +16,38 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   </button>
 );
 const SearchBar = () => {
+  const router = useRouter();
   const [manufacturer, setManufacturer] = useState("");
   const [model, setModel] = useState("");
-  const handleSearch = () => {};
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (manufacturer === "" && model === "") {
+      return alert("Please fill in the search bar");
+    }
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+  };
+
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    if (model) {
+      searchParams.set("model", model);
+    } else {
+      searchParams.delete("model");
+    }
+
+    if (manufacturer) {
+      searchParams.set("manufacturer", manufacturer);
+    } else {
+      searchParams.delete("manufacturer");
+    }
+
+    const newPathname = `${
+      window.location.pathname
+    }?${searchParams.toString()}`;
+
+    router.push(newPathname);
+  };
   return (
     <form className="searchbar" onSubmit={handleSearch}>
       <div className="searchbar__item">
@@ -42,7 +73,7 @@ const SearchBar = () => {
           placeholder="Tiguan"
           className="searchbar__input"
         />
-            <SearchButton otherClasses="sm:hidden" />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
       <SearchButton otherClasses="max-sm:hidden" />
     </form>
